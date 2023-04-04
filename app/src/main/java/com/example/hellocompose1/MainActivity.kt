26669b6c884e17cbfc9797e7f84564c8264bc046
Sodai-Komichi <83051052+Aso2101122@users.noreply.github.com>
2023-04-04 12:00:18 +0900

    チャプター1

--- app/src/main/java/com/example/hellocompose1/MainActivity.kt
+++ app/src/main/java/com/example/hellocompose1/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.hellocompose1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.hellocompose1.ui.theme.HelloCompose1Theme
 
 class MainActivity : ComponentActivity() {
@@ -18,26 +22,44 @@ class MainActivity : ComponentActivity() {
         setContent {
             HelloCompose1Theme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
         }
     }
 }
 
 @Composable
+fun MyApp(names: List<String> = listOf("World", "Compose")) {
+    Surface(
+        color = MaterialTheme.colors.background
+    ) {
+        Column () {
+            for (name in names){
+                Greeting(name)
+            }
+
+        }
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(color = MaterialTheme.colors.primary) {
+        Column(modifier = Modifier.padding(24.dp)) {
+            Text(
+                text = "Hello,",
+            )
+            Text(
+                text = name,
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     HelloCompose1Theme {
-        Greeting("Android")
+        MyApp()
     }
 }
