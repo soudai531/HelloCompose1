b3253ff2cc637c7e3e8e56f1f0df99fa0780bb39
Sodai-Komichi <83051052+Aso2101122@users.noreply.github.com>
2023-04-05 16:19:18 +0900

    Showボタンの配置

--- app/src/main/java/com/example/hellocompose1/MainActivity.kt
+++ app/src/main/java/com/example/hellocompose1/MainActivity.kt
@@ -3,13 +3,8 @@ package com.example.hellocompose1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -33,30 +28,36 @@ fun MyApp(names: List<String> = listOf("World", "Compose")) {
     Surface(
         color = MaterialTheme.colors.background
     ) {
-        Column () {
+        Column (modifier = Modifier.padding(vertical = 4.dp)) {
             for (name in names){
                 Greeting(name)
             }
-
         }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colors.primary) {
-        Column(modifier = Modifier.padding(24.dp)) {
-            Text(
-                text = "Hello,",
-            )
-            Text(
-                text = name,
-            )
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+            ) {
+                Text(text = "Hello,")
+                Text(text = name)
+            }
+            OutlinedButton(onClick = { /*TODO*/ }) {
+                Text("Show more")
+            }
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     HelloCompose1Theme {
